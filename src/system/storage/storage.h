cd9ee6acfcdff96ff154a600153fbbc00d8a81f1
Marco Mora <m.mora@intel.com>
2023-02-24 11:41:48 -0600

    Initial version to save values in memory
    Signed-off-by: Marco Mora <m.mora@intel.com>

--- src/system/storage/storage.h
+++ src/system/storage/storage.h
@@ -20,20 +20,17 @@
 #define __IRRIGATION_SYSTEM_STORAGE_STORAGE_H__
 #include <Arduino.h>
 #include <EEPROM.h>
-#include <stddef.h>
-#include <stdint.h>
 #include "../../utils/logger.h"
 
-typedef struct {
-  uint8_t templateid [20];
-  uint8_t templateName [20];
-  uint8_t authToken [40];
-  uint8_t padding [20];
-} EEPROM_CredentialStorage_t;
+#define BLYNK_TEMPLATE_ID_MAX     20  // Max number of characters
+#define BLYNK_TEMPLATE_NAME_MAX   20
+#define BLYNK_AUTH_TOKEN_MAX      40
+#define BLYNK_TEMPLATE_ID_BASE    0   // base address in bytes
+#define BLYNK_TEMPLATE_NAME_BASE  20
+#define BLYNK_AUTH_TOKEN_BASE     40
 
-#define CONF_MEM_START  sizeof(EEPROM_CredentialStorage_t)  // leave the first space for credentials
-                                                            // save the conf above this address
-
+#define CONF_MEM_START  80            // leave the first space for credentials
+                                      // save the conf above this address
 
 typedef struct {
   uint8_t hour;
@@ -41,23 +38,84 @@ typedef struct {
   uint8_t sec;
   uint8_t duration;
   uint8_t days;
-} eeprom_map_conf_time_t;
+} conf_time_t;
 
 
-class Storage
+String templateId;
+String templateName;
+String authToken;
+
+class storage
 {
 private:
-  int num_relays;
-  eeprom_map_conf_time_t relayConfigTime;
+  int num_releys;
+  conf_time_t reley;
   
 public:
-  Storage(int _num_relays);
-  ~Storage();
+  storage(int _num_releys);
+  ~storage();
   bool saveCredentials(String templateID, String templateName, String authToken);
-  bool saveConfiguration(int relayID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration, uint8_t days);
+  bool saveConfiguration(int releyID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration);
   bool getCredentials(String templateID, String templateName, String authToken);
-  bool getConfiguration(int relayID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration, uint8_t days);
+  bool getConfiguration(int releyID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration, uint8_t days);
   
 };
 
-#endif //__IRRIGATION_SYSTEM_STORAGE_STORAGE_H__
+storage::storage(int _num_releys)
+{
+  this->num_releys = _num_releys;
+  // offset for credentials, plus space requred to save the configuration for each reley
+  EEPROM.begin(CONF_MEM_START + (sizeof(conf_time_t) * _num_releys));
+}
+
+storage::~storage()
+{
+}
+
+bool storage::saveCredentials(String templateID, String templateName, String authToken)
+{
+  EEPROM.put(BLYNK_TEMPLATE_ID_BASE,templateID);
+  EEPROM.put(BLYNK_TEMPLATE_NAME_BASE, templateName);
+  EEPROM.put(BLYNK_AUTH_TOKEN_BASE, authToken);
+  EEPROM.commit();
+  return true;
+}
+
+bool storage::saveConfiguration(int releyID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration, uint8_t days)
+{
+  reley.hour = hour;
+  reley.min = minute;
+  reley.sec = second;
+  reley.duration = duration;
+  reley.days = days;
+
+  EEPROM.put(CONF_MEM_START * releyID, reley);
+  EEPROM.commit();
+
+  return true;
+}
+
+bool storage::getCredentials(String templateID, String templateName, String authToken)
+{
+  EEPROM.get(BLYNK_TEMPLATE_ID_BASE,templateID);
+  EEPROM.get(BLYNK_TEMPLATE_NAME_BASE,templateName);
+  EEPROM.get(BLYNK_AUTH_TOKEN_BASE,authToken);
+
+  logger << LOG_INFO << "templateID " << templateID << EndLine;
+  logger << LOG_INFO << "templateName " << templateName << EndLine;
+  logger << LOG_INFO << "authToken " << authToken << EndLine;
+
+  return true;
+}
+
+bool storage::getConfiguration(int releyID, uint8_t hour, uint8_t minute, uint8_t second, uint8_t duration, uint8_t days)
+{
+  EEPROM.get(CONF_MEM_START * releyID, reley);
+  hour    = reley.hour;
+  minute  = reley.min;
+  second  = reley.sec;
+  duration  = reley.duration;
+  days    = reley.days;
+}
+
+#endif __IRRIGATION_SYSTEM_STORAGE_STORAGE_H__
